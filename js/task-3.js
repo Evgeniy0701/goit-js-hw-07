14f7d26daa622b6adfee06da72ac6d38391deff6
Evgeniy0701 <kotia200456@gmail.com>
2024-01-30 12:06:23 +0200

    hwr7

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,3 @@
-<input type="text" id="name-input" placeholder="Please enter your name" />
-  <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
 
 const nameInput = document.getElementById('name-input');
--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,4 @@
-<form class="login-form" onsubmit="handleFormSubmit(event)">
-  <label>
-    Email
-    <input type="email" name="email" />
-  </label>
-  <label>
-    Password
-    <input type="password" name="password" />
-  </label>
-  <button type="submit">Log in</button>
-</form>
+
 
 
 
